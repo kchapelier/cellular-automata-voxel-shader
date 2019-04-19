2388f41c4d7b3e093a6e239c9ca20dccf8fd9a2a
kchapelier <kevin.chapelier@gmail.com>
2019-04-19 16:39:40 +0200

    Minor refactoring

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 "use strict";
 
-var parser = require('cellular-automata-rule-parser'),
-    moore = require('moore'),
-    vonNeumann = require('von-neumann'),
-    unconventionalNeighbours = require('unconventional-neighbours'),
-    generateGlsl = require('./lib/cellular-automata-voxel-shader-glsl');
+const parser = require('cellular-automata-rule-parser');
+const moore = require('moore');
+const vonNeumann = require('von-neumann');
+const unconventionalNeighbours = require('unconventional-neighbours');
+const generateGlsl = require('./lib/cellular-automata-voxel-shader-glsl');
 
-var neighbourhoodFunctions = {
-    'moore': moore,
-    'von-neumann': vonNeumann,
-    'axis': unconventionalNeighbours.axis,
-    'corner': unconventionalNeighbours.corner,
-    'edge': unconventionalNeighbours.edge,
-    'face': unconventionalNeighbours.face
+const neighbourhoodFunctions = {
+  moore: moore,
+  'von-neumann': vonNeumann,
+  axis: unconventionalNeighbours.axis,
+  corner: unconventionalNeighbours.corner,
+  edge: unconventionalNeighbours.edge,
+  face: unconventionalNeighbours.face
 };
 
 /**
@@ -21,33 +21,30 @@ var neighbourhoodFunctions = {
  * @param {Array} b Second neighbour
  * @returns {number}
  */
-var neighbourhoodSorter = function neighbourhoodSorter (a, b) {
-    a = a.join(',');
-    b = b.join(',');
-    return a > b ? 1 : a < b ? -1 : 0;
+function neighbourhoodSorter (a, b) {
+  a = a.join(',');
+  b = b.join(',');
+  return a > b ? 1 : a < b ? -1 : 0;
+}
+
+function getNeighbourhood (neighbourhoodType, neighbourhoodRange) {
+  neighbourhoodType = !!neighbourhoodFunctions[neighbourhoodType] ? neighbourhoodType : 'moore';
+  neighbourhoodRange = neighbourhoodRange || 1;
+
+  const neighbourhood = neighbourhoodFunctions[neighbourhoodType](neighbourhoodRange, 3);
+  neighbourhood.sort(neighbourhoodSorter);
+
+  return neighbourhood;
+}
+
+module.exports = function generate (ruleString, outOfBoundValue) {
+  const rule = parser(ruleString);
+
+  if (!rule) {
+    throw Error('Invalid ruleString : "' + ruleString + '"');
+  }
+
+  const neighbourhood = getNeighbourhood(rule.neighbourhoodType, rule.neighbourhoodRange);
+
+  return generateGlsl(rule, neighbourhood, outOfBoundValue);
 };
-
-var getNeighbourhood = function getNeighbourhood (neighbourhoodType, neighbourhoodRange) {
-    neighbourhoodType = !!neighbourhoodFunctions[neighbourhoodType] ? neighbourhoodType : 'moore';
-    neighbourhoodRange = neighbourhoodRange || 1;
-
-    var neighbourhood = neighbourhoodFunctions[neighbourhoodType](neighbourhoodRange, 3);
-    neighbourhood.sort(neighbourhoodSorter);
-
-    return neighbourhood;
-};
-
-var generate = function generate (ruleString, outOfBoundValue) {
-    var rule = parser(ruleString),
-        neighbourhood;
-
-    if (!rule) {
-        throw Error('Invalid ruleString : "' + ruleString + '"');
-    }
-
-    neighbourhood = getNeighbourhood(rule.neighbourhoodType, rule.neighbourhoodRange);
-
-    return generateGlsl(rule, neighbourhood, outOfBoundValue);
-};
-
-module.exports = generate;
